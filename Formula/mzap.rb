3744d4f4ba6eecef43d61c2cdef43142ce593650
hahwul <hahwul@gmail.com>
2020-08-23 17:04:14 +0900

    Brew formula update for mzap version v1.1.0

--- Formula/mzap.rb
+++ Formula/mzap.rb
@@ -2,16 +2,16 @@
 class Mzap < Formula
   desc "Multiple target ZAP scanning / mzap is a tool for scanning N*N in ZAP."
   homepage "https://www.hahwul.com"
-  version "1.0.0"
+  version "1.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/hahwul/mzap/releases/download/v1.0.0/mzap_1.0.0_darwin_amd64.tar.gz"
-    sha256 "f9ba076b4d7373a5b347626e3c2b6f4a2426877b7e30d0cba11e66564fe0cbba"
+    url "https://github.com/hahwul/mzap/releases/download/v1.1.0/mzap_1.1.0_darwin_amd64.tar.gz"
+    sha256 "f7f0b6fcfd795fe1b8a2b6bc90f05ea75e8cc5efa58dbbe97e31967857b6225d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/hahwul/mzap/releases/download/v1.0.0/mzap_1.0.0_linux_amd64.tar.gz"
-      sha256 "3e2ff41d3a04d5ccba32acdad5c730083e4cf3674ffb638552f16aed21c01948"
+      url "https://github.com/hahwul/mzap/releases/download/v1.1.0/mzap_1.1.0_linux_amd64.tar.gz"
+      sha256 "f2b538a2489f3c92234ff94be6773abb4dd78f84858e79af10fedeea6c808526"
     end
   end
 
